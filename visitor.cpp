3aef76176272c32181bcd2de4a20c32f8b4993f6
lws597 <lwscode@163.com>
2020-10-08 18:24:08 +0800

    2020/10/08

--- visitor.cpp
+++ visitor.cpp
@@ -1,56 +1,92 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-class B;
-class C;
-class D;
-class E;
-class F;
+class B; // +5
+class C; // *2
+class D; // -4
+class E; // +6
+class F; // -7
 
 class A {
-public:
-    class IVistor {
-    public:
+public :
+    class IVisitor {
+    public :
         virtual void visit(A *) = 0;
         virtual void visit(B *) = 0;
         virtual void visit(C *) = 0;
         virtual void visit(D *) = 0;
         virtual void visit(E *) = 0;
+        virtual void visit(F *) = 0;
     };
-    virtual void Accept(IVistor *vis) {
+    virtual void Accept(IVisitor *vis) {
         vis->visit(this);
     }
     virtual ~A() {}
 };
 
-class B : public A {};
-class C : public A {};
-class D : public A {};
-class E : public A {};
-class F : public A {};
-
-class PriVisitor : public A::IVistor {
-public: 
-    virtual void visit(B *obj) {
-        cout << "class B" << endl;
-    }
-    virtual void visit(C *obj) {
-        cout << "class C" << endl;
-    }
-    virtual void visit(D *obj) {
-        cout << "class D" << endl;
-    }
-    virtual void visit(E *obj) {
-        cout << "class E" << endl;
-    }
-    virtual void visit(F *obj) {
-        cout << "class F" << endl;
+class B : public A {
+public:
+    void Accept(IVisitor *vis) {
+        vis->visit(this);
     }
 };
 
-class CalVisitor : public A::IVistor {
+class C : public A {
 public:
-    CalVisitor(int val = 1) : val(val) {}
+    void Accept(IVisitor *vis) {
+        vis->visit(this);
+    }
+};
+
+class D : public A {
+public:
+    void Accept(IVisitor *vis) {
+        vis->visit(this);
+    }
+};
+
+class E : public A {
+public:
+    void Accept(IVisitor *vis) {
+        vis->visit(this);
+    }
+};
+
+class F : public A {
+public:
+    void Accept(IVisitor *vis) {
+        vis->visit(this);
+    }
+};
+
+class OutputVisitor : public A::IVisitor {
+public:
+    virtual void visit(A *obj) {
+        cout << "this is father class A" << endl;
+    }
+    virtual void visit(B *obj) {
+        cout << "this is a class B object" << endl;
+    }
+    virtual void visit(C *obj) {
+        cout << "this is a class C object" << endl;
+    }
+    virtual void visit(D *obj) {
+        cout << "this is a class D object" << endl;
+    }
+    virtual void visit(E *obj) {
+        cout << "this is a class E object" << endl;
+    }
+    virtual void visit(F *obj) {
+        cout << "this is a class F object" << endl;
+    }
+};
+
+class CalcVisitor : public A::IVisitor {
+public :
+    int val;
+    CalcVisitor(int val = 1) : val(val) {}
+    virtual void visit(A *obj) {}
     virtual void visit(B *obj) {
         val += 5;
     }
@@ -66,7 +102,6 @@ public:
     virtual void visit(F *obj) {
         val -= 7;
     }
-    int val;
 };
 
 int main() {
@@ -81,10 +116,12 @@ int main() {
             case 4: arr[i] = new F(); break;
         }
     }
-    CalVisitor vis;
+    OutputVisitor vis1;
+    CalcVisitor vis2;
     for (int i = 0; i < 10; i++) {
-        vis.visit(arr[i]);
+        arr[i]->Accept(&vis1);
+        arr[i]->Accept(&vis2);
     }
-    cout << vis.val << endl;
+    cout << vis2.val << endl;
     return 0;
 }
